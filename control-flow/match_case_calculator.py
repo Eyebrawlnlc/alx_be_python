c38366cf5d1b00f0b3cdcc0188708c6bc1ce0d1c
Nishay Hira <123361531+Eyebrawlnlc@users.noreply.github.com>
2025-07-17 10:32:58 +0200

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,17 +1,17 @@
 number1 = float(input("Enter the first number: "))
 number2 = float(input("Enter the second number: "))
-operation = input("Choose the operation (+, -, *, /): ")
+operations = input("Choose the operation (+, -, *, /): ")
 
 
-if operation == "+":
+if operations == "+":
     result = number1 + number2
     print(result)
-elif operation == "-":
+elif operations == "-":
     result = number1 - number2
     print(result)
-elif operation == "*":
+elif operations == "*":
     result = number1 * number2
     print(result)
-elif operation == "/":
+elif operations == "/":
     result = number1 / number2
     print(result)
